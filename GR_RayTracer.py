715eeef8bbf4c6f4a707d2b0dc44bd1772d9f5fc
MosheLevy20 <71517199+MosheLevy20@users.noreply.github.com>
2023-03-18 10:33:42 -0400

    Update GR_RayTracer.py

--- GR_RayTracer.py
+++ GR_RayTracer.py
@@ -46,37 +46,7 @@ for x in range(4):
 
 	chrisC.append(row)
 
-# t, r, theta, phi = symbols("t r theta phi", cls=Function)
-# coords_s = [t(s), r(s), theta(s), phi(s)]
 
-
-# def solveGeodesicEq():
-	
-# 	eqs = []
-# 	for mu in range(4):
-# 		#second diff of coords[mu] with respect to s
-# 		LHS = diff(coords_s[mu],s,2)
-# 		RHS = 0
-# 		#print("RHS: ", RHS)
-# 		for alpha in range(4):
-# 			for beta in range(4):
-# 				RHS += chrisC[mu][alpha][beta]*diff(coords_s[alpha], s)*diff(coords_s[beta], s)
-# 		#replace coords with their expressions in terms of s
-# 		for i in range(4):
-# 			RHS = RHS.subs(coords[i], coords_s[i])
-
-# 		#solve RHS = 0 for coords[mu](s)
-# 		#print(RHS)
-# 		eqs.append(Eq(RHS,LHS))
-# 	#solve the system of equations
-# 	print(eqs)
-# 	sol = dsolve(eqs, coords_s)
-# 	return sol
-# sol = solveGeodesicEq()
-# #sols = [(t(s), (-sqrt((-2.0*diff(r(s), s)*diff(t(s), s) + diff(t(s), (s, 2)))*diff(t(s), (s, 2))) + diff(t(s), (s, 2)))/diff(t(s), (s, 2)), 2.0*atan((-sqrt((-2.0*diff(r(s), s)*diff(t(s), s) + diff(t(s), (s, 2)))*diff(t(s), (s, 2)))*diff(phi(s), (s, 2)) - 1.4142135623731*sqrt((-((-2.0*diff(r(s), s)*diff(t(s), s) + diff(t(s), (s, 2)))*diff(t(s), (s, 2)))**0.5*diff(phi(s), s)**2*diff(theta(s), s)**2 - 2.0*((-2.0*diff(r(s), s)*diff(t(s), s) + diff(t(s), (s, 2)))*diff(t(s), (s, 2)))**0.5*diff(phi(s), s)*diff(phi(s), (s, 2))*diff(r(s), s) - ((-2.0*diff(r(s), s)*diff(t(s), s) + diff(t(s), (s, 2)))*diff(t(s), (s, 2)))**0.5*diff(phi(s), (s, 2))**2 + 2.0*diff(phi(s), s)**2*diff(r(s), s)**2*diff(t(s), (s, 2)) - diff(phi(s), s)**2*diff(r(s), s)*diff(t(s), s)*diff(theta(s), s)**2 + diff(phi(s), s)**2*diff(t(s), (s, 2))*diff(theta(s), s)**2 + 2.0*diff(phi(s), s)*diff(phi(s), (s, 2))*diff(r(s), s)*diff(t(s), (s, 2)) - diff(phi(s), (s, 2))**2*diff(r(s), s)*diff(t(s), s) + diff(phi(s), (s, 2))**2*diff(t(s), (s, 2)))*diff(t(s), (s, 2))) + 2.0*diff(phi(s), s)*diff(r(s), s)*diff(t(s), (s, 2)) + diff(phi(s), (s, 2))*diff(t(s), (s, 2)))/((-((-2.0*diff(r(s), s)*diff(t(s), s) + diff(t(s), (s, 2)))*diff(t(s), (s, 2)))**0.5 + diff(t(s), (s, 2)))*diff(phi(s), s)*diff(theta(s), s))), phi(s)), (t(s), (-sqrt((-2.0*diff(r(s), s)*diff(t(s), s) + diff(t(s), (s, 2)))*diff(t(s), (s, 2))) + diff(t(s), (s, 2)))/diff(t(s), (s, 2)), 2.0*atan((-sqrt((-2.0*diff(r(s), s)*diff(t(s), s) + diff(t(s), (s, 2)))*diff(t(s), (s, 2)))*diff(phi(s), (s, 2)) + 1.4142135623731*sqrt((-((-2.0*diff(r(s), s)*diff(t(s), s) + diff(t(s), (s, 2)))*diff(t(s), (s, 2)))**0.5*diff(phi(s), s)**2*diff(theta(s), s)**2 - 2.0*((-2.0*diff(r(s), s)*diff(t(s), s) + diff(t(s), (s, 2)))*diff(t(s), (s, 2)))**0.5*diff(phi(s), s)*diff(phi(s), (s, 2))*diff(r(s), s) - ((-2.0*diff(r(s), s)*diff(t(s), s) + diff(t(s), (s, 2)))*diff(t(s), (s, 2)))**0.5*diff(phi(s), (s, 2))**2 + 2.0*diff(phi(s), s)**2*diff(r(s), s)**2*diff(t(s), (s, 2)) - diff(phi(s), s)**2*diff(r(s), s)*diff(t(s), s)*diff(theta(s), s)**2 + diff(phi(s), s)**2*diff(t(s), (s, 2))*diff(theta(s), s)**2 + 2.0*diff(phi(s), s)*diff(phi(s), (s, 2))*diff(r(s), s)*diff(t(s), (s, 2)) - diff(phi(s), (s, 2))**2*diff(r(s), s)*diff(t(s), s) + diff(phi(s), (s, 2))**2*diff(t(s), (s, 2)))*diff(t(s), (s, 2))) + 2.0*diff(phi(s), s)*diff(r(s), s)*diff(t(s), (s, 2)) + diff(phi(s), (s, 2))*diff(t(s), (s, 2)))/((-((-2.0*diff(r(s), s)*diff(t(s), s) + diff(t(s), (s, 2)))*diff(t(s), (s, 2)))**0.5 + diff(t(s), (s, 2)))*diff(phi(s), s)*diff(theta(s), s))), phi(s)), (t(s), (sqrt((-2.0*diff(r(s), s)*diff(t(s), s) + diff(t(s), (s, 2)))*diff(t(s), (s, 2))) + diff(t(s), (s, 2)))/diff(t(s), (s, 2)), 2.0*atan((sqrt((-2.0*diff(r(s), s)*diff(t(s), s) + diff(t(s), (s, 2)))*diff(t(s), (s, 2)))*diff(phi(s), (s, 2)) - 1.4142135623731*sqrt((((-2.0*diff(r(s), s)*diff(t(s), s) + diff(t(s), (s, 2)))*diff(t(s), (s, 2)))**0.5*diff(phi(s), s)**2*diff(theta(s), s)**2 + 2.0*((-2.0*diff(r(s), s)*diff(t(s), s) + diff(t(s), (s, 2)))*diff(t(s), (s, 2)))**0.5*diff(phi(s), s)*diff(phi(s), (s, 2))*diff(r(s), s) + ((-2.0*diff(r(s), s)*diff(t(s), s) + diff(t(s), (s, 2)))*diff(t(s), (s, 2)))**0.5*diff(phi(s), (s, 2))**2 + 2.0*diff(phi(s), s)**2*diff(r(s), s)**2*diff(t(s), (s, 2)) - diff(phi(s), s)**2*diff(r(s), s)*diff(t(s), s)*diff(theta(s), s)**2 + diff(phi(s), s)**2*diff(t(s), (s, 2))*diff(theta(s), s)**2 + 2.0*diff(phi(s), s)*diff(phi(s), (s, 2))*diff(r(s), s)*diff(t(s), (s, 2)) - diff(phi(s), (s, 2))**2*diff(r(s), s)*diff(t(s), s) + diff(phi(s), (s, 2))**2*diff(t(s), (s, 2)))*diff(t(s), (s, 2))) + 2.0*diff(phi(s), s)*diff(r(s), s)*diff(t(s), (s, 2)) + diff(phi(s), (s, 2))*diff(t(s), (s, 2)))/((((-2.0*diff(r(s), s)*diff(t(s), s) + diff(t(s), (s, 2)))*diff(t(s), (s, 2)))**0.5 + diff(t(s), (s, 2)))*diff(phi(s), s)*diff(theta(s), s))), phi(s)), (t(s), (sqrt((-2.0*diff(r(s), s)*diff(t(s), s) + diff(t(s), (s, 2)))*diff(t(s), (s, 2))) + diff(t(s), (s, 2)))/diff(t(s), (s, 2)), 2.0*atan((sqrt((-2.0*diff(r(s), s)*diff(t(s), s) + diff(t(s), (s, 2)))*diff(t(s), (s, 2)))*diff(phi(s), (s, 2)) + 1.4142135623731*sqrt((((-2.0*diff(r(s), s)*diff(t(s), s) + diff(t(s), (s, 2)))*diff(t(s), (s, 2)))**0.5*diff(phi(s), s)**2*diff(theta(s), s)**2 + 2.0*((-2.0*diff(r(s), s)*diff(t(s), s) + diff(t(s), (s, 2)))*diff(t(s), (s, 2)))**0.5*diff(phi(s), s)*diff(phi(s), (s, 2))*diff(r(s), s) + ((-2.0*diff(r(s), s)*diff(t(s), s) + diff(t(s), (s, 2)))*diff(t(s), (s, 2)))**0.5*diff(phi(s), (s, 2))**2 + 2.0*diff(phi(s), s)**2*diff(r(s), s)**2*diff(t(s), (s, 2)) - diff(phi(s), s)**2*diff(r(s), s)*diff(t(s), s)*diff(theta(s), s)**2 + diff(phi(s), s)**2*diff(t(s), (s, 2))*diff(theta(s), s)**2 + 2.0*diff(phi(s), s)*diff(phi(s), (s, 2))*diff(r(s), s)*diff(t(s), (s, 2)) - diff(phi(s), (s, 2))**2*diff(r(s), s)*diff(t(s), s) + diff(phi(s), (s, 2))**2*diff(t(s), (s, 2)))*diff(t(s), (s, 2))) + 2.0*diff(phi(s), s)*diff(r(s), s)*diff(t(s), (s, 2)) + diff(phi(s), (s, 2))*diff(t(s), (s, 2)))/((((-2.0*diff(r(s), s)*diff(t(s), s) + diff(t(s), (s, 2)))*diff(t(s), (s, 2)))**0.5 + diff(t(s), (s, 2)))*diff(phi(s), s)*diff(theta(s), s))), phi(s))]
-# print(len(sol))
-# print(sol)
-# quit()
 def ricci(g,i,j):
 	total = 0
 	
@@ -95,13 +65,6 @@ def ricci(g,i,j):
 
 
 
-
-
-#print(chrisC)
-#how to include braces in python fstring? 
-#
-#
-
 cppFile = "#include <cmath>\n#include \"chrisC.h\"\nvoid calcChrisC(double r, double theta, double phi, double t, double chrisC1[4][4][4]){\n"
 for index, row in enumerate(chrisC):
 	for index2, column in enumerate(row):
@@ -119,7 +82,7 @@ with open('chrisC.cpp', 'w') as f:
 	f.write(cppFile)
 
 #run the following: g++ -fopenmp -o testGR testGR.cpp chrisC.cpp Ray.cpp Vector3.cpp -std=c++11 using subprocess
-subprocess.run(["/opt/homebrew/Cellar/gcc/12.2.0/bin/aarch64-apple-darwin21-g++-12", "-fopenmp", "-o", "testGR", "testGR.cpp", "chrisC.cpp", "Ray.cpp", "Vector3.cpp", "-std=c++11"])
+subprocess.run(["g++", "-fopenmp", "-o", "testGR", "testGR.cpp", "chrisC.cpp", "Ray.cpp", "Vector3.cpp", "-std=c++11"])
 #time the next line
 start = time.time()
 subprocess.run(["./testGR"])
@@ -136,65 +99,6 @@ def cartesian(pos):
 	y = radius*sin(theta)*sin(phi)
 	z = radius*cos(theta)
 	return [x,y,z]
-
-
-
-
-# def polar(x,y):
-# 	return [math.sqrt(x**2+y**2),math.atan2(y,x)]
-
-# class ray(object):
-# 	"""docstring for ray"""
-# 	def __init__(self, pos, vel):
-# 		self.pos = pos
-# 		self.vel = vel
-# 		self.worldLine = []
-# 	def geodesic(self):
-# 		self.worldLine.append(cartesian(self.pos))
-		
-# 		dim = len(self.pos)
-# 		newVel = []
-# 		for lamda, l in enumerate(self.vel):
-# 			a = 0
-# 			for mu in range(dim):
-# 				for nu in range(dim):
-# 					cVal = chrisC[lamda][mu][nu].subs(r, self.pos[1])
-# 					a -= cVal*self.vel[mu]*self.vel[nu]
-# 					#print(chrisC[0][0][1].subs(r, 3.5),'this')
-# 			newVel.append(l+a*dTau)
-# 		self.vel = newVel
-
-# 		for lamda, l in enumerate(self.pos):
-# 			self.pos[lamda] += self.vel[lamda]*dTau
-
-
-
-# lines = []
-# for i in range(1,2):
-# 	print(i)
-# 	#r0,theta0 = polar(5,1.45+i*0.02)
-# 	r0, theta0 = [100*rs, 0]
-# 	#vr0,vtheta0 = polar(-0.1*i,0)
-	
-# 	ray1 = ray([0,r0,theta0], [1,0,0.00041])
-# 	for j in range(3000):
-# 		ray1.geodesic()
-# 		if abs(ray1.pos[1]) <= 1.1*rs:
-# 			break;
-# 	lines.append(ray1.worldLine)
-# print(2*G*M)
-# #print(lines[1])
-# for i in lines:
-# 	X = [j[0] for j in i]
-# 	Y = [j[1] for j in i]
-# 	#plt.plot(np.cumsum(X), np.cumsum(Y))
-# 	#for j in i:
-# 	plt.scatter(X,Y)
-# 	plt.plot(X,Y)
-	
-# plt.show()
-
-
 
 
 
